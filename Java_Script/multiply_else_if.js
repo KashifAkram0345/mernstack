bdf7b820c9edc51a78b0d48b0c68b7b74c9aa0bb
KashifAkram0345 <m.kashif6833@gmail.com>
2025-06-29 14:50:51 +0500

    Conditional Operator

---
+++ Java_Script/multiply_else_if.js
@@ -0,0 +1,11 @@
+let num = 5;
+
+if (num == 1) {
+    console.log('A');
+} else if (num == 2) {
+    console.log('B');
+} else if (num == 3) {
+    console.log('C');
+} else {
+    console.log("Error");
+}
