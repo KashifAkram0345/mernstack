006ce7688872151a52769085c285e0eacf9c8336
KashifAkram0345 <m.kashif6833@gmail.com>
2025-06-30 17:41:56 +0500

    string

---
+++ Java_Script/string.js
@@ -0,0 +1,20 @@
+// let FirstName = 'Hello';
+// let LastName = 'Bye';
+// console.log(FirstName);
+
+// let Name = `This is
+//  Laptop`;
+// console.log(Name, Name.length);
+// let x = 'English';
+// let y = 'urdu';
+
+// console.log(x, x.length);         
+// console.log(y, y.length);        
+// console.log(x.toUpperCase());     
+// console.log(y.toLowerCase());     
+// let str = "Kashif";
+// console.log(str.substring(3));
+
+
+
+
