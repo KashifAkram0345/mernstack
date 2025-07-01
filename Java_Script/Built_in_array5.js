5b53178dae90c844fd7e655e07b070968f276038
KashifAkram0345 <m.kashif6833@gmail.com>
2025-07-01 17:16:40 +0500

    Array_Built_in_splince

---
+++ Java_Script/Built_in_array5.js
@@ -0,0 +1,11 @@
+let arr = ['Github', 1, true];
+console.log(arr); 
+
+let brr = new Array('Linkdin', 2, false);
+brr.push(10);
+brr.push(30);  
+brr.push(40);
+brr.push(50);
+brr.push(100);
+console.log(brr.splice(2,4,'Akram'));
+console.log(brr);
