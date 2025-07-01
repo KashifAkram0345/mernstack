cdaf01e4f8adceb07bb7f64a525672e61907b626
KashifAkram0345 <m.kashif6833@gmail.com>
2025-07-01 14:46:19 +0500

    Array

---
+++ Java_Script/array2.js
@@ -0,0 +1,11 @@
+let obj={
+    Name: "Kashif Akram" ,
+    age: "16" ,
+    height: "5ft 6inc" ,
+    weight: "42" ,
+    bio : function(){
+        console.log("Hy Github");
+    }
+};
+console.log(obj);
+obj.bio();
