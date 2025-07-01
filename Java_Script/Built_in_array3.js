d83ed27d652bf4d24400a4825576853bd813640f
KashifAkram0345 <m.kashif6833@gmail.com>
2025-07-01 16:30:03 +0500

    Array_Built_in_ shift

---
+++ Java_Script/Built_in_array3.js
@@ -0,0 +1,11 @@
+let arr = ['Github', 1, true];
+console.log(arr);  // Output: [ 'Github', 1, true ]
+
+let brr = new Array('Linkdin', 2, false);
+brr.push(10);
+brr.push(30);
+brr.push(40);
+brr.push(50);
+brr.push(100);
+console.log(brr);          
+console.log(brr.shift());  
