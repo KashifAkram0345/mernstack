6eaf1ce2ca849388d04e146b3235afd2866456f4
KashifAkram0345 <m.kashif6833@gmail.com>
2025-07-01 16:43:19 +0500

    Array_Built_in_unshift

---
+++ Java_Script/Built_in_array4.js
@@ -0,0 +1,13 @@
+let arr = ['Github', 1, true];
+console.log(arr); 
+
+let brr = new Array('Linkdin', 2, false);
+brr.push(10);
+brr.push(30);  
+brr.push(40);
+brr.push(50);
+brr.push(100);
+
+console.log(brr.unshift('Kashif'));
+console.log(brr); 
+
