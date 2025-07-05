cbd14b4678839dd29b6da29a28d83a739ec1cde4
KashifAkram0345 <m.kashif6833@gmail.com>
2025-07-05 09:36:27 +0500

    Class Object

---
+++ Java_Script/class_obj.js
@@ -0,0 +1,19 @@
+
+
+  class Human{
+    // Properties
+
+    age =13;
+    height =5;
+    weight =84;
+    running(){
+        console.log("I am running Function");
+
+    }
+     walking(){
+        console.log("I am walking function");
+     }
+  }
+
+  let obj= new Human();
+  obj.walking();
