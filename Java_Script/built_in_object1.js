e63ffdfaf2955837f5962402b1edc23d423a0179
KashifAkram0345 <m.kashif6833@gmail.com>
2025-07-07 12:48:17 +0500

    Class Object

---
+++ Java_Script/built_in_object1.js
@@ -0,0 +1,12 @@
+console.log("Hello Github");
+
+console.log(Math.max(10, 80, 96, 76, 87));
+
+console.log(Math.max(10, 80, 96, 76, 87));
+
+console.log(Math.min(5, 9, 1, 8, 5, 34));
+
+console.log(Math.PI);
+
+console.log(Math.ceil(9.8));
+
