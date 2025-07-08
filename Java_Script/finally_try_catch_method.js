39f6118ffcce3433918ef317c3c05ff2d3bb2678
KashifAkram0345 <m.kashif6833@gmail.com>
2025-07-08 15:47:05 +0500

    Finally Try Catch Method

---
+++ Java_Script/finally_try_catch_method.js
@@ -0,0 +1,15 @@
+
+
+
+try{
+    console.log("Try Method");
+    console.log("Try Method Start");
+    console.log(x);
+}
+catch(error){
+    console.log("your Error is Here");
+}
+
+finally{
+    console.log("I AM KING  ");
+}
