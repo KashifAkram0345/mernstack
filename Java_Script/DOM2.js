e1131c01e1a95eb2edaef71f7b091dd75b870e79
KashifAkram0345 <m.kashif6833@gmail.com>
2025-07-11 11:53:08 +0500

    Changing html code into j.s code

---
+++ Java_Script/DOM2.js
@@ -0,0 +1,4 @@
+let mydiv = document.querySelector('#div1');
+let newElement = document.createElement('span');
+newElement.textContent = "My name is kashif";
+div1.insertAdjacentElement('beforebegin', newElement);
