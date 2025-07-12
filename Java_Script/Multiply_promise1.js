25e867934b61b12dba774446c27a0459085d73e3
KashifAkram0345 <m.kashif6833@gmail.com>
2025-07-12 10:34:31 +0500

    Multiply Promises

---
+++ Java_Script/Multiply_promise1.js
@@ -0,0 +1,9 @@
+let promises1 = new Promise((resolve, reject) => {
+    setTimeout(resolve,1000 , "Kashif");
+})
+let promises2 = new Promise((resolve, reject) => {
+    setTimeout(resolve,1000 , "Kashif");
+})
+let promises3=  new Promise((resolve, reject) => {
+    setTimeout(resolve,1000 , "Kashif");
+})
