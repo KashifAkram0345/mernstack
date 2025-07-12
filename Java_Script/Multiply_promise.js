b59987b4b903faf91d898f6322e6a0027e9b9304
KashifAkram0345 <m.kashif6833@gmail.com>
2025-07-12 10:25:54 +0500

    Then Catch Method

---
+++ Java_Script/Multiply_promise.js
@@ -0,0 +1,14 @@
+
+
+
+let promise1 = new Promise((resolve, reject) => {
+    setTimeout(resolve,1000 , "Kashif");
+})
+
+// let promisess2 = new Promise((resolve, reject) => {
+//     setTimeout(resolve, "Akram", 1000);
+// })
+
+// let promisess3 = new Promise((resolve, reject) => {
+//     setTimeout(resolve, "Github", 1000);
+// })
