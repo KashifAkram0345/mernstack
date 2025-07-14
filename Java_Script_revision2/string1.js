df6a5b80aeed8fb92921653fcc35f1b9763bafa8
KashifAkram0345 <m.kashif6833@gmail.com>
2025-07-14 16:18:31 +0500

    String1

---
+++ Java_Script_revision2/string1.js
@@ -0,0 +1,15 @@
+
+
+// print
+let op1 = "English";
+let op2 = "Urdu";
+console.log(op1, op2);
+// check lenght
+console.log(op1.length, op2.length);
+// substring
+console.log(op1.substring(3), op2.substring(4));
+
+let sentence = "Hello Jee Kaise Ho Sary";
+let words = sentence.split(' ');
+console.log(words);
+
