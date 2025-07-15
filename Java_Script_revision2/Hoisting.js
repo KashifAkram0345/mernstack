6a0fab4fb638793834dff0ff40cb9339da20a58f
KashifAkram0345 <m.kashif6833@gmail.com>
2025-07-15 14:07:54 +0500

    Hoisting

---
+++ Java_Script_revision2/Hoisting.js
@@ -0,0 +1,15 @@
+{
+    {
+        
+        
+        let x = 10;
+        console.log(x);
+    
+    
+
+        sayName("Kashif Akram");
+        function sayName(Name) {
+            console.log(Name);
+        }
+    }
+}
