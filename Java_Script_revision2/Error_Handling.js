631f84a137a9adec298ad3fa44dd7ea844d24df0
KashifAkram0345 <m.kashif6833@gmail.com>
2025-07-15 21:57:47 +0500

    Error

---
+++ Java_Script_revision2/Error_Handling.js
@@ -0,0 +1,28 @@
+
+// Error Handling
+// Compile Time Error
+
+// //let x = 10;
+// console.log(;
+
+// Run Time Error
+    
+
+
+// try catch Method
+
+try {
+    console.log("I'm Try Method");
+
+    console.log(x);
+    console.log("Your try method");
+    
+} catch (error) {
+    console.log("I'm catch block");
+
+}
+
+
+finally {
+    console.log("Finally Block Run EveryTime");
+}
