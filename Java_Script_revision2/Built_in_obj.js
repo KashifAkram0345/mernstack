eaabbfa4b51e7859bbaf5b531ae2398ae3547735
KashifAkram0345 <m.kashif6833@gmail.com>
2025-07-15 20:56:38 +0500

    Object Built in

---
+++ Java_Script_revision2/Built_in_obj.js
@@ -0,0 +1,13 @@
+
+
+console.log("Kashif");
+// Math Object
+
+console.log(Math.PI);
+console.log(Math.max(20, 20, 0, 400));
+console.log(Math.min(9, 8, 900, 5, 3, 9, 2));
+console.log(Math.abs(-23));
+
+
+let crr = new Date();
+console.log(crr);
